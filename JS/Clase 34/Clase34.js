f842dc867f158554d93a30885d16c5f831a74a31
Andres David Elizalde Peralta <andep210399@gmail.com>
2023-03-17 20:30:38 -0500

    Clase34

---
+++ JS/Clase 34/Clase34.js
@@ -0,0 +1,24 @@
+//estructura if
+// if (condicion){
+//     proceso
+// }
+
+let x = Number(prompt("Ingrese un valor entero",0));
+if (x > 10){
+    alert("El numero es mayor que 10");
+}
+
+if (x > 10){
+    alert("El numero es mayor que 10");
+} else{
+    alert("El numero es menor que 10");
+}
+
+
+if (x > 10){
+    alert("El numero es mayor que 10");
+} else if(x = 10){
+    alert("El numero es 10");
+} else {
+    alert("El numero es menor a 10")
+}

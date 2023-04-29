6a6608233ae93b1e5d09a9eb5010d7ce87828945
Andres David Elizalde Peralta <andep210399@gmail.com>
2023-04-28 22:23:31 -0500

    Prueba Clase 54

--- JS/Clase 53/problema-1-1/js/gesnombas.js
+++ JS/Clase 53/problema-1-1/js/gesnombas.js
@@ -1,4 +1,4 @@
-const formulario = document.querySelector('form');
+const formulario = document.querySelector("form");
 formulario.addEventListener("submit", agregarEmpleadoNomina);
 
 /*
@@ -20,231 +20,228 @@ const mapEstadistica = new Map();
 const salMinimo = 1_160_000;
 
 function agregarEmpleadoNomina(event) {
-    event.preventDefault(); // Evita que el formulario se envíe
+  event.preventDefault(); // Evita que el formulario se envíe
 
-    try {
-        // Crea un objeto FormData con el formulario
-        const datosFormulario = new FormData(formulario);
-        console.log(datosFormulario);
-        capturarInformacion(datosFormulario);
-        // capturarInformacion()
-        // const ultEmpleado = vEmplados.at(-1);
-        // calcularNominaEmpleado(ultEmpleado)
-    } catch (ex) {
-        console.log(ex.message);
-        formulario.cedula.focus();
-    }
-
-
+  try {
+    // Crea un objeto FormData con el formulario
+    const datosFormulario = new FormData(formulario);
+    console.log(datosFormulario);
+    capturarInformacion(datosFormulario);
+    // capturarInformacion()
     // const ultEmpleado = vEmplados.at(-1);
-    // if ( === false) return;
+    // calcularNominaEmpleado(ultEmpleado)
+  } catch (ex) {
+    console.log(ex.message);
+    formulario.cedula.focus();
+  }
 
-    // if (calcularEstadisticaNomina(ultEmpleado) === false) return;
+  // const ultEmpleado = vEmplados.at(-1);
+  // if ( === false) return;
 
-    // console.log("mapEstadistica", mapEstadistica);
+  // if (calcularEstadisticaNomina(ultEmpleado) === false) return;
 
-    // Agregar empleado a tabla de nomina
+  // console.log("mapEstadistica", mapEstadistica);
 
-    // Agregar empleados
+  // Agregar empleado a tabla de nomina
 
-    // Modifica totales en el pie de la tabla
+  // Agregar empleados
 
-    // publicar y modificar tabla de estadistic
-    // publicarEstadisticaHTML();
+  // Modifica totales en el pie de la tabla
 
-    e.stopPropagation();
+  // publicar y modificar tabla de estadistic
+  // publicarEstadisticaHTML();
+
+  e.stopPropagation();
 }
 
-
 function capturarInformacion(datosFormulario) {
-    const mapEmpleados = new Map();
-    const mapDatEmpleados = new Map();
+  const mapEmpleados = new Map();
+  const mapDatEmpleados = new Map();
 
-    // validar y agregar cedula al vector datos empleados
-    let cedula = datosFormulario.get("cedula");
-    if (cedula.length === 0)
-        throw new Error("La cédula no es valida");
+  // validar y agregar cedula al vector datos empleados
+  let cedula = datosFormulario.get("cedula");
+  if (cedula.length === 0) throw new Error("La cédula no es valida");
 
-    // validar y agregar nombre al vector datos empleados
-    let nombre = datosFormulario.get("nombre");
-    if (nombre.length === 0)
-        throw new Error("El nombre no es válida");
-    mapDatEmpleados.set("nombre", nombre);
+  // validar y agregar nombre al vector datos empleados
+  let nombre = datosFormulario.get("nombre");
+  if (nombre.length === 0) throw new Error("El nombre no es válida");
+  mapDatEmpleados.set("nombre", nombre);
 
-    // validar y agragar la categoria
-    let abrCategoria = datosFormulario.get("categoria");
-    if (abrCategoria === "NN")
-        throw new Error("La cateoria no es valida");
-    mapDatEmpleados.set("abrcategoria", abrCategoria);
-    mapDatEmpleados.set("categoria", formulario.categoria.options[formulario.categoria.selectedIndex].text)
+  // validar y agragar la categoria
+  let abrCategoria = datosFormulario.get("categoria");
+  if (abrCategoria === "NN") throw new Error("La cateoria no es valida");
+  mapDatEmpleados.set("abrcategoria", abrCategoria);
+  mapDatEmpleados.set(
+    "categoria",
+    formulario.categoria.options[formulario.categoria.selectedIndex].text
+  );
 
-    //Agregar los el dic de datos de empledos al mapa de empleados cuya llave es la cedula
-    mapEmpleados.set(cedula, mapDatEmpleados);
+  //Agregar los el dic de datos de empledos al mapa de empleados cuya llave es la cedula
+  mapEmpleados.set(cedula, mapDatEmpleados);
 
-    //Agregar al vector de empleado el mapa de empeados
-    vEmplados.push(mapDatEmpleados);
+  //Agregar al vector de empleado el mapa de empeados
+  vEmplados.push(mapDatEmpleados);
 }
 
 function calcularSalBrutoBonificacion(ultEmpleado) {
-    const mapCategorias = new Map([
-        ["AA", [1, 100_000]],
-        ["AB", [1.2, 100_000]],
-        ["TA", [1.5, 150_000]],
-        ["TB", [2, 150_000]],
-        ["PA", [2.5, 200_000]],
-        ["PB", [3, 250_000]],
-        ["DA", [4, 500_000]],
-        ["DB", [4.5, 1_000_000]],
-        ["GD", [6, 2_000_000]],
-        ["GG", [10, 5_000_000]],
-    ]);
+  const mapCategorias = new Map([
+    ["AA", [1, 100_000]],
+    ["AB", [1.2, 100_000]],
+    ["TA", [1.5, 150_000]],
+    ["TB", [2, 150_000]],
+    ["PA", [2.5, 200_000]],
+    ["PB", [3, 250_000]],
+    ["DA", [4, 500_000]],
+    ["DB", [4.5, 1_000_000]],
+    ["GD", [6, 2_000_000]],
+    ["GG", [10, 5_000_000]],
+  ]);
 
-    const catEmpleado = ultEmpleado.get("abrcategoria");
-    // Calcular salario bruto
-    const salbruto = mapCategorias.get(catEmpleado)[0] * salMinimo;
-    // Calcurlar bonificacion
-    const bonificacion = mapCategorias.get(catEmpleado)[1];
+  const catEmpleado = ultEmpleado.get("abrcategoria");
+  // Calcular salario bruto
+  const salbruto = mapCategorias.get(catEmpleado)[0] * salMinimo;
+  // Calcurlar bonificacion
+  const bonificacion = mapCategorias.get(catEmpleado)[1];
 
-    if (salbruto === NaN) {
-        ultEmpleado.set("salbruto", 0);
-        ultEmpleado.set("bonificacion", 0);
-        alert("Ha ocurrido un error al calcular el salario bruto del empleado");
-        console.log(ultEmpleado);
-        return false;
-    }
-    ultEmpleado.set("salbruto", salbruto);
-    ultEmpleado.set("bonificacion", bonificacion);
+  if (salbruto === NaN) {
+    ultEmpleado.set("salbruto", 0);
+    ultEmpleado.set("bonificacion", 0);
+    alert("Ha ocurrido un error al calcular el salario bruto del empleado");
+    console.log(ultEmpleado);
+    return false;
+  }
+  ultEmpleado.set("salbruto", salbruto);
+  ultEmpleado.set("bonificacion", bonificacion);
 
-    return true;
+  return true;
 }
 
 function calcularAuxTx(ultEmpleado) {
-    const salbruto = ultEmpleado.get("salbruto");
+  const salbruto = ultEmpleado.get("salbruto");
 
-    if (salbruto < 1.5 * salMinimo) ultEmpleado.set("auxtx", 400_000);
-    else ultEmpleado.set("auxtx", 0);
+  if (salbruto < 1.5 * salMinimo) ultEmpleado.set("auxtx", 400_000);
+  else ultEmpleado.set("auxtx", 0);
 
-    return true;
+  return true;
 }
 
 function calcularEPSPension(ultEmpleado) {
-    const salbruto = ultEmpleado.get("salbruto");
-    const eps = salbruto * 0.04;
-    const pension = salbruto * 0.04;
+  const salbruto = ultEmpleado.get("salbruto");
+  const eps = salbruto * 0.04;
+  const pension = salbruto * 0.04;
 
-    ultEmpleado.set("eps", eps);
-    ultEmpleado.set("pension", pension);
+  ultEmpleado.set("eps", eps);
+  ultEmpleado.set("pension", pension);
 
-    return true;
+  return true;
 }
 
 function calcularSalarioNetoTotalizar(ultEmpleado) {
-    const salbruto = ultEmpleado.get("salbruto");
-    const auxtx = ultEmpleado.get("auxtx");
-    const bonificacion = ultEmpleado.get("bonificacion");
-    const eps = ultEmpleado.get("eps");
-    const pension = ultEmpleado.get("pension");
+  const salbruto = ultEmpleado.get("salbruto");
+  const auxtx = ultEmpleado.get("auxtx");
+  const bonificacion = ultEmpleado.get("bonificacion");
+  const eps = ultEmpleado.get("eps");
+  const pension = ultEmpleado.get("pension");
 
-    // calcular salario neto
-    ultEmpleado.set("salneto", salbruto + auxtx + bonificacion - eps - pension);
+  // calcular salario neto
+  ultEmpleado.set("salneto", salbruto + auxtx + bonificacion - eps - pension);
 
-    // totalizar nomina
-    for (let llave of ultEmpleado.keys()) {
-        if (mapTotalNomina.has(llave))
-            mapTotalNomina.set(
-                llave,
-                mapTotalNomina.get(llave) + ultEmpleado.get(llave)
-            );
-        else mapTotalNomina.set(llave, ultEmpleado.get(llave));
-    }
+  // totalizar nomina
+  for (let llave of ultEmpleado.keys()) {
+    if (mapTotalNomina.has(llave))
+      mapTotalNomina.set(
+        llave,
+        mapTotalNomina.get(llave) + ultEmpleado.get(llave)
+      );
+    else mapTotalNomina.set(llave, ultEmpleado.get(llave));
+  }
 
-    mapTotalNomina.delete("nombre");
-    mapTotalNomina.delete("categoria");
-    mapTotalNomina.delete("abrcategoria");
+  mapTotalNomina.delete("nombre");
+  mapTotalNomina.delete("categoria");
+  mapTotalNomina.delete("abrcategoria");
 
-    return true;
+  return true;
 }
 
 function calcularNominaEmpleado(ultEmpleado) {
-    if (calcularSalBrutoBonificacion(ultEmpleado) === false) return false;
+  if (calcularSalBrutoBonificacion(ultEmpleado) === false) return false;
 
-    if (calcularAuxTx(ultEmpleado) === false) return false;
+  if (calcularAuxTx(ultEmpleado) === false) return false;
 
-    if (calcularEPSPension(ultEmpleado) === false) return false;
+  if (calcularEPSPension(ultEmpleado) === false) return false;
 
-    if (calcularSalarioNetoTotalizar(ultEmpleado) === false) return false;
+  if (calcularSalarioNetoTotalizar(ultEmpleado) === false) return false;
 
-    console.log(vEmplados);
-    console.log(mapTotalNomina);
+  console.log(vEmplados);
+  console.log(mapTotalNomina);
 
-    return true;
+  return true;
 }
 
 function calcularEstadisticaNomina(ultEmpleado) {
-    // cant empleados
-    mapEstadistica.set("cantempl", vEmplados.length);
+  // cant empleados
+  mapEstadistica.set("cantempl", vEmplados.length);
 
-    const nombre = ultEmpleado.get("nombre");
-    const salneto = ultEmpleado.get("salneto");
+  const nombre = ultEmpleado.get("nombre");
+  const salneto = ultEmpleado.get("salneto");
 
-    // empleado mas gana
-    console.log("entro0", mapEstadistica);
-    if (mapEstadistica.has("emplmasgana")) {
-        console.log("entro1", mapEstadistica);
-        if (salneto > mapEstadistica.get("emplmasgana")[1]) {
-            console.log("entro2", mapEstadistica);
-            mapEstadistica.set("emplmasgana", [nombre, salneto]);
-            console.log("entro3", mapEstadistica);
-        }
-    } else mapEstadistica.set("emplmasgana", [nombre, salneto]);
+  // empleado mas gana
+  console.log("entro0", mapEstadistica);
+  if (mapEstadistica.has("emplmasgana")) {
+    console.log("entro1", mapEstadistica);
+    if (salneto > mapEstadistica.get("emplmasgana")[1]) {
+      console.log("entro2", mapEstadistica);
+      mapEstadistica.set("emplmasgana", [nombre, salneto]);
+      console.log("entro3", mapEstadistica);
+    }
+  } else mapEstadistica.set("emplmasgana", [nombre, salneto]);
 
-    // empleado menos gana
-    if (mapEstadistica.has("emplmenosgana")) {
-        if (salneto < mapEstadistica.get("emplmenosgana")[1])
-            mapEstadistica.set("emplmenosgana", [nombre, salneto]);
-    } else mapEstadistica.set("emplmenosgana", [nombre, salneto]);
+  // empleado menos gana
+  if (mapEstadistica.has("emplmenosgana")) {
+    if (salneto < mapEstadistica.get("emplmenosgana")[1])
+      mapEstadistica.set("emplmenosgana", [nombre, salneto]);
+  } else mapEstadistica.set("emplmenosgana", [nombre, salneto]);
 
-    // sumatoria nomina
-    if (mapEstadistica.has("totalnomina")) {
-        mapEstadistica.set(
-            "totalnomina",
-            salneto + mapEstadistica.get("totalnomina")
-        );
-    } else mapEstadistica.set("totalnomina", salneto);
-
-    // promedio de nomina
+  // sumatoria nomina
+  if (mapEstadistica.has("totalnomina")) {
     mapEstadistica.set(
-        "promnomina",
-        mapEstadistica.get("totalnomina") / mapEstadistica.get("cantempl")
+      "totalnomina",
+      salneto + mapEstadistica.get("totalnomina")
     );
+  } else mapEstadistica.set("totalnomina", salneto);
 
-    return true;
+  // promedio de nomina
+  mapEstadistica.set(
+    "promnomina",
+    mapEstadistica.get("totalnomina") / mapEstadistica.get("cantempl")
+  );
+
+  return true;
 }
 
 function publicarEstadisticaHTML() {
-    document.getElementById("est-cant-empl").textContent =
-        mapEstadistica.get("cantempl");
+  document.getElementById("est-cant-empl").textContent =
+    mapEstadistica.get("cantempl");
 
-    // publicar empleado que menos gana
-    let nombre = mapEstadistica.get("emplmenosgana")[0];
-    let salario = Math.round(mapEstadistica.get("emplmenosgana")[1]);
-    document.getElementById(
-        "est-empl-menosgana"
-    ).textContent = `${nombre} ($${salario})`;
+  // publicar empleado que menos gana
+  let nombre = mapEstadistica.get("emplmenosgana")[0];
+  let salario = Math.round(mapEstadistica.get("emplmenosgana")[1]);
+  document.getElementById(
+    "est-empl-menosgana"
+  ).textContent = `${nombre} ($${salario})`;
 
-    // promedio nommina
-    let prom = Math.round(mapEstadistica.get("promnomina"));
-    document.getElementById("est-prom-nomina").textContent = `$${prom}`;
+  // promedio nommina
+  let prom = Math.round(mapEstadistica.get("promnomina"));
+  document.getElementById("est-prom-nomina").textContent = `$${prom}`;
 
-    // publicar empleado que mas gana
-    nombre = mapEstadistica.get("emplmasgana")[0];
-    salario = Math.round(mapEstadistica.get("emplmasgana")[1]);
-    document.getElementById(
-        "est-empl-masgana"
-    ).textContent = `${nombre} ($${salario})`;
+  // publicar empleado que mas gana
+  nombre = mapEstadistica.get("emplmasgana")[0];
+  salario = Math.round(mapEstadistica.get("emplmasgana")[1]);
+  document.getElementById(
+    "est-empl-masgana"
+  ).textContent = `${nombre} ($${salario})`;
 
-    // Total de la nomina
-    let total = Math.round(mapEstadistica.get("totalnomina"));
-    document.getElementById("est-total-nom").textContent = `$${total}`;
+  // Total de la nomina
+  let total = Math.round(mapEstadistica.get("totalnomina"));
+  document.getElementById("est-total-nom").textContent = `$${total}`;
 }
-
---
+++ JS/Clase 54/Prueba/Prueba.js
@@ -0,0 +1,206 @@
+class Producto {
+  #codigo;
+  #descripcion;
+  #precioDeCompra;
+  #precioDeVenta;
+  #cantidadEnBodega;
+  #canMinBod;
+  #canMaxInv;
+  #porcDesc;
+  #tipo;
+  constructor(
+    codigo,
+    descripcion,
+    precioDeCompra,
+    precioDeVenta,
+    cantidadEnBodega,
+    canMinBod,
+    canMaxInv,
+    porcDesc,
+    tipo
+  ) {
+    this.#codigo = codigo;
+    this.#descripcion = descripcion;
+    this.#precioDeCompra = precioDeCompra;
+    this.#precioDeVenta = precioDeVenta;
+    this.#cantidadEnBodega = cantidadEnBodega;
+    this.#canMinBod = canMinBod;
+    this.#canMaxInv = canMaxInv;
+    this.#porcDesc = porcDesc;
+    this.#tipo = tipo;
+  }
+  //encapsulamiento
+  get Codigo() {
+    return this.#codigo;
+  }
+  set Codigo(newCodigo) {
+    this.#codigo = newCodigo;
+  }
+  get Descripcion() {
+    return this.#descripcion;
+  }
+  set Descripcion(newDescripcion) {
+    this.#descripcion = newDescripcion;
+  }
+  //metodos
+  SolicitarPedido() {
+    if (this.#canMinBod >= this.#cantidadEnBodega) {
+      return true;
+    }
+    return false;
+  }
+  CalcularTotalAPagar() {
+    let valor;
+    if (SolicitarPedido() === true) {
+      valor =
+        this.#precioDeCompra * (this.#canMinBod - this.#cantidadEnBodega + 1);
+    }
+    return valor;
+  }
+  Mostrar() {
+    crearTarjeta();
+  }
+}
+//Clases hijos
+class PrendasDeVestir extends Producto {
+  #talla;
+  #permitePlanchado;
+  constructor(
+    codigo,
+    descripcion,
+    precioDeCompra,
+    precioDeVenta,
+    cantidadEnBodega,
+    canMinBod,
+    canMaxInv,
+    porcDesc,
+    tipo,
+    talla,
+    permitePlanchado
+  ) {
+    super(
+      codigo,
+      descripcion,
+      precioDeCompra,
+      precioDeVenta,
+      cantidadEnBodega,
+      canMinBod,
+      canMaxInv,
+      porcDesc,
+      tipo
+    );
+    this.#talla = talla;
+    this.#permitePlanchado = permitePlanchado;
+  }
+}
+
+class Calzado extends Producto {
+  #talla;
+  constructor(
+    codigo,
+    descripcion,
+    precioDeCompra,
+    precioDeVenta,
+    cantidadEnBodega,
+    canMinBod,
+    canMaxInv,
+    porcDesc,
+    tipo,
+    talla
+  ) {
+    super(
+      codigo,
+      descripcion,
+      precioDeCompra,
+      precioDeVenta,
+      cantidadEnBodega,
+      canMinBod,
+      canMaxInv,
+      porcDesc,
+      tipo
+    );
+    this.#talla = talla;
+  }
+}
+const agregarAdicional = document.getElementById("tipo");
+agregarAdicional.addEventListener("click", (e) => {
+  if (agregarAdicional.value === "2") {
+    document.getElementById("permPlan").setAttribute("disabled", "");
+  } else {
+    if (document.getElementById("permPlan").getAttribute("disabled") != null)
+      document.getElementById("permPlan").removeAttribute("disabled");
+  }
+});
+
+let cantProdIngre = 0;
+let cantPrendVestIngre = 0;
+let cantProdAPedir = 0;
+let mayCant = 0;
+let mayoPorGan = 0;
+
+const formulario = document.querySelector("form");
+formulario.addEventListener("submit", AgregarPrenda);
+
+function AgregarPrenda(event) {
+  event.preventDefault();
+  let datosPrenda = new FormData(formulario);
+  crearTarjeta(datosPrenda);
+}
+
+function crearTarjeta(datosPrenda) {
+  cantProdIngre += 1;
+  let porGanan =
+    (datosPrenda.get("precioVenta") / datosPrenda.get("precioCompra")) * 100;
+  let template = document.getElementById("temp3").content;
+  let fragment = document.createDocumentFragment();
+  fragment = document.getElementById("here");
+  let clone = template.cloneNode(true);
+  clone.getElementById("codHere").textContent = datosPrenda.get("codigo");
+  if (datosPrenda.get("tipo") === "1") {
+    cantPrendVestIngre += 1;
+    clone.getElementById("tipoHere").textContent = "Prendas de Vestir";
+  } else {
+    if (mayCant < Number(datosPrenda.get("cantBodega"))) {
+      mayCantDescr = datosPrenda.get("descripcion");
+      document.getElementById(
+        "calMayCantCod"
+      ).textContent = `Cantidad: ${datosPrenda.get("cantBodega")}`;
+      document.getElementById(
+        "calMayCantDescr"
+      ).textContent = `Codigo: ${datosPrenda.get("codigo")}`;
+    }
+    clone.getElementById("tipoHere").textContent = "Calzado";
+  }
+
+  clone.getElementById("cantHere").textContent = datosPrenda.get("cantBodega");
+  clone.getElementById("precHere").textContent = `Precio venta $${datosPrenda
+    .get("precioVenta")
+    .toLocaleString("es")}`;
+  clone.getElementById("decrpHere").textContent =
+    datosPrenda.get("descripcion");
+  if (
+    Number(datosPrenda.get("cantBodega")) <=
+    Number(datosPrenda.get("cantMinBodega"))
+  ) {
+    cantProdAPedir += 1;
+    clone.querySelector("div").style.backgroundColor = "#808080";
+  }
+  fragment.appendChild(clone);
+  if (mayoPorGan < porGanan) {
+    document.getElementById("mayPorCod").textContent =
+      datosPrenda.get("codigo");
+    document.getElementById("mayPorDesc").textContent =
+      datosPrenda.get("descripcion");
+    document.getElementById("mayPorComp").textContent = `${datosPrenda
+      .get("precioCompra")
+      .toLocaleString("es")}`;
+    document.getElementById("mayPorVentas").textContent = `${datosPrenda
+      .get("precioVenta")
+      .toLocaleString("es")}`;
+    document.getElementById("mayPorPor").textContent = porGanan;
+  }
+  document.getElementById("here").content = fragment;
+  document.getElementById("cantProd").textContent = cantProdIngre;
+  document.getElementById("cantPrend").textContent = cantPrendVestIngre;
+  document.getElementById("cantProdReq").textContent = cantProdAPedir;
+}
